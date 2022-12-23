3bc58aa8e05cf67a2381e4cede5f99ff2ec71ac7
francescotrudu <francescotrudu84@gmail.com>
2022-12-23 17:33:17 +0100

    Add comments

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,11 +1,17 @@
+<!-- Inserire JavaScript -->
 <script>
+
 </script>
 
+<!-- Inserire html -->
 <template>
     <h1>La mia prima app con Vite!</h1>
-
 </template>
 
+<!-- Inserire css -->
 <style scoped>
-
+h1 {
+    text-align: center;
+    font-size: 3rem;
+}
 </style>
