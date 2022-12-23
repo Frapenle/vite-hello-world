bb6f1120ed6a3a67b6ebea5af8e1e856a81ac43a
francescotrudu <francescotrudu84@gmail.com>
2022-12-23 17:32:50 +0100

    Add image

---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <img src="https://images.unsplash.com/photo-1671363332482-d3d2e53808d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80"
+        alt="wallpaper">
+
+</template>
+
+<style scoped>
+
+</style>
